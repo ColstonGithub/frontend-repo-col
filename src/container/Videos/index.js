2b81101d3e62290cf41bd468403dc614c023a563
Preetam Kumar <preetamkumar3232@gmail.com>
2023-07-22 18:27:58 +0530

    added

--- src/container/Videos/index.js
+++ src/container/Videos/index.js
@@ -20,7 +20,9 @@ import Header from "components/SearchBar/Header";
 import videoBanner from "assets/videoBanner.png";
 import FMTypography from "components/FMTypography/FMTypography";
 
+// Styles for the component and its elements using Material UI's styling library - makeStyles() hook is
 const useStyles = makeStyles((theme) => ({
+
   headerContainer: {
     display: "flex",
     justifyContent: "center",
@@ -32,12 +34,14 @@ const useStyles = makeStyles((theme) => ({
     width: "100%",
     borderRadius: theme.spacing(3),
   },
+
   videoContainer: {
     display: "grid",
     gridTemplateColumns: "repeat(auto-fit, minmax(280px, 1fr))",
     gap: "2rem",
     padding: theme.spacing(3, 0),
   },
+
   videoCard: {
     border: `1px solid ${theme.palette.grey[300]}`,
     borderRadius: "24px !important",
@@ -49,9 +53,11 @@ const useStyles = makeStyles((theme) => ({
       transform: "scale(1.02)",
     },
   },
+
   videoThumbnail: {
     position: "relative",
   },
+
   playButton: {
     position: "absolute",
     top: "50%",
@@ -65,14 +71,17 @@ const useStyles = makeStyles((theme) => ({
       backgroundColor: "#rgba(0, 0, 0, 0.3) !important",
     },
   },
+
   videoInfo: {
     padding: theme.spacing(2),
   },
+
   videoTitle: {
     fontSize: "1.2rem !important",
     fontWeight: "bold",
     marginBottom: theme.spacing(1),
   },
+
   videoDescription: {
     fontSize: theme.typography.pxToRem(14),
     color: theme.palette.text.secondary,
@@ -97,15 +106,18 @@ const useStyles = makeStyles((theme) => ({
       borderColor: "#C02121 !important",
     },
   },
+
 }));
 
 const Videos = () => {
+
   const classes = useStyles();
   const [videos, setVideos] = useState([]);
   const [pageToken, setPageToken] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const [hasMoreVideos, setHasMoreVideos] = useState(true);
   const responsiveMobile = useMediaQuery("(max-width: 600px)");
+
 
   useEffect(() => {
     fetchData();
@@ -115,7 +127,7 @@ const Videos = () => {
     try {
       setIsLoading(true);
       const response = await axios.get(
-        `http://localhost:5000/api/video/getvideos?pageToken=${pageToken}`
+        `http://64.227.150.49:5000/api/video/getvideos?pageToken=${pageToken}`
       );
       if (response.data) {
         const { videoData, nextPageToken } = response.data;
@@ -143,12 +155,20 @@ const Videos = () => {
   const handlePlayVideo = (videoId) => {
     window.open(`https://www.youtube.com/watch?v=${videoId}`, "_blank");
   };
+
   useEffect(() => {
     window.scrollTo({
       top: 0,
       behavior: "smooth",
     });
   }, []);
+
+
+
+
+
+
+
   return (
     <>
       <Header />
@@ -178,7 +198,7 @@ const Videos = () => {
         <Grid item xs={12} style={{ paddingTop: "2rem" }}>
           <Box className={classes.videoContainer}>
             {videos &&
-              videos.map((video) => (
+              videos?.map((video) => (
                 <Card
                   key={video?.id?.videoId}
                   className={classes.videoCard}
@@ -187,8 +207,8 @@ const Videos = () => {
                   <div className={classes.videoThumbnail}>
                     <CardMedia
                       component="img"
-                      image={video.snippet.thumbnails.medium.url}
-                      alt={video.snippet.title}
+                      image={video?.snippet?.thumbnails?.medium?.url}
+                      alt={video?.snippet?.title}
                     />
                     <IconButton className={classes.playButton}>
                       <FaYoutube size={32} className={classes.youtubeIcon} />
@@ -196,13 +216,13 @@ const Videos = () => {
                   </div>
                   <CardContent className={classes.videoInfo}>
                     <Typography className={classes.videoTitle}>
-                      {video.snippet.title}
+                      {video?.snippet?.title}
                     </Typography>
                     <Typography
                       className={classes.videoDescription}
                       variant="body2"
                     >
-                      {video.snippet.description}
+                      {video?.snippet?.description}
                     </Typography>
                   </CardContent>
                 </Card>
@@ -233,246 +253,3 @@ const Videos = () => {
 };
 
 export default Videos;
-
-// const useStyles = makeStyles((theme) => ({
-//   headerContainer: {
-//     display: "flex",
-//     justifyContent: "center",
-//     paddingTop: theme.breakpoints.up("sm") ? "1.8rem" : "1rem",
-//     paddingBottom: theme.breakpoints.up("sm") ? "1.8rem" : "1rem",
-//   },
-
-//   bannerImage: {
-//     width: "100%",
-//     borderRadius: theme.spacing(3),
-//   },
-//   videoContainer: {
-//     display: "grid",
-//     gridTemplateColumns: "repeat(auto-fit, minmax(280px, 1fr))",
-//     gap: "2rem",
-//     padding: theme.spacing(3, 0),
-//   },
-//   videoCard: {
-//     border: `1px solid ${theme.palette.grey[300]}`,
-//     borderRadius: "24px !important",
-//     overflow: "hidden",
-//     boxShadow: theme.shadows[2],
-//     cursor: "pointer",
-//     transition: "transform 0.2s ease",
-//     "&:hover": {
-//       transform: "scale(1.02)",
-//     },
-//   },
-//   videoThumbnail: {
-//     position: "relative",
-//   },
-//   playButton: {
-//     position: "absolute",
-//     top: "50%",
-//     left: "50%",
-//     transform: "translate(-50%, -50%)",
-//     backgroundColor: "rgba(0, 0, 0, 0.5) !important",
-//     borderRadius: "50%",
-//     padding: theme.spacing(2),
-//     color: theme.palette.common.white,
-//     "&:hover": {
-//       backgroundColor: "#rgba(0, 0, 0, 0.3) !important",
-//     },
-//   },
-//   videoInfo: {
-//     padding: theme.spacing(2),
-//   },
-//   videoTitle: {
-//     fontSize: "1.2rem !important",
-//     fontWeight: "bold",
-//     marginBottom: theme.spacing(1),
-//   },
-//   videoDescription: {
-//     fontSize: theme.typography.pxToRem(14),
-//     color: theme.palette.text.secondary,
-//   },
-
-//   youtubeIcon: {
-//     color: "#fff",
-//     transition: "color 0.3s ease",
-//     "&:hover": {
-//       color: "#ff0000",
-//     },
-//   },
-// }));
-
-// const Videos = () => {
-//   const classes = useStyles();
-//   const [videos, setVideos] = useState([]);
-//   const [pageToken, setPageToken] = useState("");
-//   const responsiveMobile = useMediaQuery("(max-width: 600px)");
-//   const [isLoading, setIsLoading] = useState(false);
-
-//   useEffect(() => {
-//     fetchData();
-//   }, []);
-
-//   // useEffect(() => {
-//   //   const observer = new IntersectionObserver(handleObserver, {
-//   //     root: null,
-//   //     rootMargin: "0px",
-//   //     threshold: 1.0,
-//   //   });
-
-//   //   if (containerRef.current) {
-//   //     observer.observe(containerRef.current);
-//   //   }
-
-//   //   return () => {
-//   //     if (containerRef.current) {
-//   //       observer.unobserve(containerRef.current);
-//   //     }
-//   //   };
-//   // }, [videos]);
-
-//   // const handleObserver = (entries) => {
-//   //   const target = entries[0];
-//   //   if (target.isIntersecting && !isLoading) {
-//   //     fetchData();
-//   //   }
-//   // };
-
-//   const [hasMoreVideos, setHasMoreVideos] = useState(true);
-
-//   const handleLoadMore = async () => {
-//     if (pageToken) {
-//       try {
-//         setIsLoading(true);
-//         const response = await axios.get(
-//           `http://localhost:5000/api/video/getvideos?pageToken=${pageToken}`
-//         );
-
-//         if (response.data) {
-//           const { videoData, nextPageToken } = response.data;
-
-//           if (!nextPageToken) {
-//             // If there is no nextPageToken, there are no more videos to fetch
-//             setHasMoreVideos(false);
-//           }
-
-//           // Append new videos to the existing ones
-//           setVideos((prevVideos) => [...prevVideos, ...videoData]);
-//           setPageToken(nextPageToken);
-//         } else {
-//           toast.error("Error fetching videos");
-//         }
-//       } catch (err) {
-//         toast.error("Error fetching videos", err);
-//       } finally {
-//         setIsLoading(false);
-//       }
-//     }
-//   };
-//   const fetchData = async () => {
-//     try {
-//       setIsLoading(true);
-//       const response = await axios.get(
-//         `http://localhost:5000/api/video/getvideos?pageToken=${pageToken}`
-//       );
-//       console.log("response ", response);
-//       if (response.data) {
-//         const { videoData, nextPageToken } = response.data;
-//         setVideos((prevVideos) => [...prevVideos, ...videoData]);
-//         setPageToken(nextPageToken);
-//       } else {
-//         toast.error("Error fetching videos");
-//       }
-//     } catch (err) {
-//       toast.error("Error fetching videos", err);
-//     } finally {
-//       setIsLoading(false);
-//     }
-//   };
-
-//   const handlePlayVideo = (videoId) => {
-//     window.open(`https://www.youtube.com/watch?v=${videoId}`, "_blank");
-//   };
-
-//   return (
-//     <>
-//       <Header />
-//       <Box className={classes.headerContainer}>
-//         <FMTypography
-//           displayText={"Videos"}
-//           styleData={{
-//             fontWeight: "600",
-//             fontSize: "2.8rem",
-//             textAlign: "center",
-//           }}
-//         />
-//       </Box>
-
-//       <Grid container sx={{ padding: "0 3.2rem 3.2rem 3.2rem" }}>
-//         {/* Banner Image */}
-//         <Grid item xs={12}>
-//           <Box className={classes.bannerImage}>
-//             <img
-//               src={videoBanner}
-//               alt="youtube banner"
-//               className={classes.bannerImage}
-//               style={{ height: !responsiveMobile ? "650px" : "62vw" }}
-//             />
-//           </Box>
-//         </Grid>
-
-//         {/* Video Cards */}
-//         <Grid item xs={12} style={{ paddingTop: "2rem" }}>
-//           <Box className={classes.videoContainer}>
-//             {videos &&
-//               videos?.map((video) => (
-//                 <Card
-//                   key={video?.id?.videoId}
-//                   className={classes.videoCard}
-//                   onClick={() => handlePlayVideo(video?.id?.videoId)}
-//                 >
-//                   <div className={classes.videoThumbnail}>
-//                     <CardMedia
-//                       component="img"
-//                       image={video.snippet.thumbnails.medium.url}
-//                       alt={video.snippet.title}
-//                     />
-//                     <IconButton className={classes.playButton}>
-//                       <FaYoutube size={32} className={classes.youtubeIcon} />
-//                     </IconButton>
-//                   </div>
-//                   <CardContent className={classes.videoInfo}>
-//                     <Typography className={classes.videoTitle}>
-//                       {video.snippet.title}
-//                     </Typography>
-
-//                     <Typography
-//                       className={classes.videoDescription}
-//                       variant="body2"
-//                     >
-//                       {video.snippet.description}
-//                     </Typography>
-//                   </CardContent>
-//                 </Card>
-//               ))}
-//             {/* Load More Button */}
-//             {hasMoreVideos && (
-//               <Box display="flex" justifyContent="center" padding="2rem">
-//                 <Button
-//                   variant="contained"
-//                   color="primary"
-//                   onClick={handleLoadMore}
-//                 >
-//                   Load More
-//                 </Button>
-//               </Box>
-//             )}
-//           </Box>
-//         </Grid>
-//       </Grid>
-//       <ToastContainer />
-//       <Footer />
-//     </>
-//   );
-// };
-
-// export default Videos;
